a2a0c85504239c91cd221f9bee555859007c8399
LZephyr <lzf.lzephyr@gmail.com>
2018-06-06 16:54:22 +0800

    install

--- RxTemplate/install.sh
+++ RxTemplate/install.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
 
+# curl https://raw.githubusercontent.com/L-Zephyr/static_resource/master/RxTemplate/install.sh | /bin/sh
+
 cd ~/
 curl -o ./RxTemplate.zip https://raw.githubusercontent.com/L-Zephyr/static_resource/master/RxTemplate/Rx%20View%20Model.xctemplate.zip
+~/Library/Developer/Xcode/Templates
 unzip -o RxTemplate.zip -d ~/Library/Developer/Xcode/Templates/File\ Templates
 rm RxTemplate.zip
