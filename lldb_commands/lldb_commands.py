810ee957bd2ae9791305f477ca462fda4f4977cf
L-Zephyr <lzf.lzephyr@gmail.com>
2018-08-05 17:02:53 +0800

    lldb commands script

---
+++ lldb_commands/lldb_commands.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import lldb
+import re
+
+def __lldb_init_module(debugger, internal_dict):
+  debugger.HandleCommand('command script add -f lldb_commands.traceclass traceclass')
+
+# ------------------
+# 1. [traceclass]: Print all method calls in the specified class.
+# ------------------
+
+def traceclass_action(frame, location, dict):
+  print(frame.GetFunctionName())
+  frame.GetThread().GetProcess().Continue()
+
+def traceclass(debugger, command, result, internal_dict):
+  if not command:
+    print("Error: class not specify")
+    return
+
+  res = lldb.SBCommandReturnObject()
+
+  # set breakpoint
+  debugger.GetCommandInterpreter().HandleCommand("br set -r \[" + command + " .*\]", res)
+  # get the breakpoint list
+  debugger.GetCommandInterpreter().HandleCommand("br list", res) 
+  # get the number of br we just set
+  breakpoint = re.compile(r"^\d+", re.M).findall(res.GetOutput()).pop()
+    
+  if not breakpoint:
+    print("Error: fail to set breakpoint!")
+    return
+
+  # add break point action
+  debugger.GetCommandInterpreter().HandleCommand("br command add " + breakpoint + " -F lldb_commands.traceclass_action", res)
