ebd48652fdbffcf05565b434168aefff6626249a
L-Zephyr <lzf.lzephyr@gmail.com>
2018-10-16 20:16:11 +0800

    update script

--- Drafter/install.sh
+++ Drafter/install.sh
@@ -4,7 +4,7 @@ cd ~/
 mkdir ./.drafter_tmp
 cd ./drafter_tmp
 curl "https://raw.githubusercontent.com/L-Zephyr/static_resource/master/Drafter/Drafter.zip" -o ./drafter.zip
-unzip drafter.zip
+unzip -o drafter.zip
 cp -f -r ./drafter/Template/ ~/.drafter
 cp -f ./drafter/drafter /usr/local/bin/drafter
 cd ~/
